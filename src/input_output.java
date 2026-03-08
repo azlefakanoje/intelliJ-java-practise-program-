65ae2e6ead8ce2e7ada2be5feae1f7145bd7620f
azlefa711 <azlefa8@gmail.com>
2026-03-09 01:45:31 +0530

    init commit

---
+++ src/input_output.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner; //input statement
+
+public class input_output {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner (System.in);  //input statement
+        System.out.println("enter the value of a="); //output statement
+        int a = scanner.nextInt(); //input statement
+        System.out.println("value of a="+a); //output statement
+    }
+}
